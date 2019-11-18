c3a8c54853c3cddd13c64ea6dcdb86826e7acb22
01Clarian <clarionnorth@gmail.com>
2019-11-18 08:49:11 -0500

    Updates

---
+++ precache-manifest.2b173d1e9f265acc400bcd1308e6b970.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00ca5aa8716a5bf87ebed2b92b9d227",
+    "url": "/weather-algorithm-application/index.html"
+  },
+  {
+    "revision": "80fa1c62f99dad762821",
+    "url": "/weather-algorithm-application/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3725854d581a153a4d60",
+    "url": "/weather-algorithm-application/static/js/2.445a6fbf.chunk.js"
+  },
+  {
+    "revision": "80fa1c62f99dad762821",
+    "url": "/weather-algorithm-application/static/js/main.06ce7341.chunk.js"
+  },
+  {
+    "revision": "bf5a6ca8348877e32761",
+    "url": "/weather-algorithm-application/static/js/runtime-main.c0d85653.js"
+  }
+]);
